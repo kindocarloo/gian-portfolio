5b0cd141b9a9198e1541172b443326873e886ade
kindocarloo <24-1-00321@vsu.edu.ph>
2026-09-02 02:31:17 +0800

    feat: implemment hero-section

---
+++ features/about/data.ts
@@ -0,0 +1,10 @@
+export const aboutData = {
+  greeting: "Hi, I am",
+  name: "GIAN CARLO J. SUICO",
+  title: "Aspiring Software Engineer",
+  paragraphs: [
+    "I am a Computer Science student at Visayas State University studying full-stack web development and machine learning.",
+    "Driven by a passion for building innovative solutions to real-world problems. I am highly interested in the intersection of machine learning and cybersecurity to build intelligent, secure, and resilient systems",
+    "When I am not in the code editor, you can usually find me watching sports, playing Manor Lords, or reading again Monster."
+  ]
+};
