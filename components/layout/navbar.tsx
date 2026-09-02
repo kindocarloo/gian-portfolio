a307b8c34241f0f6307f4257e498d43b33e88cbb
kindocarloo <24-1-00321@vsu.edu.ph>
2026-09-02 20:28:55 +0800

    refactor: resize navbar links

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -10,11 +10,12 @@ export function Navbar() {
 
                 <div className="flex items-center gap-6 mr-6">
                 <nav className = "hidden md:flex gap-4">
-                    <a href="#about" className="text-sm font-medium hover:underline hover:underline-offset-4">About</a>
-                    <a href="#projects" className="text-sm font-medium hover:underline hover:underline-offset-4">Projects</a>
-                    <a href="#experiences" className="text-sm font-medium hover:underline hover:underline-offset-4">Experience</a>
-                    <a href="#skills" className="text-sm font-medium hover:underline hover:underline-offset-4">Toolbox</a>
-                    <a href="#contact" className="text-sm font-medium hover:underline hover:underline-offset-4">Contact</a>
+                    <a href="#about" className="text-xs font-medium hover:underline hover:underline-offset-4">About</a>
+                    <a href="#projects" className="text-xs font-medium hover:underline hover:underline-offset-4">Projects</a>
+                    <a href="#experiences" className="text-xs font-medium hover:underline hover:underline-offset-4">Experience</a>
+                    <a href="#education" className="text-xs font-medium hover:underline hover:underline-offset-4">Education</a>
+                    <a href="#skills" className="text-xs font-medium hover:underline hover:underline-offset-4">Toolbox</a>
+                    <a href="#contact" className="text-xs font-medium hover:underline hover:underline-offset-4">Contact</a>
                 </nav>
                     <ThemeToggle />
                 </div>
